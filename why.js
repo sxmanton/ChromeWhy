abb24eadcd7e9fccf0aac88f3bad441d5cebafa6
sxmanton <sxmanton@bu.edu>
2015-05-20 19:30:04 -0400

    Ask again every 5 url changes in same tab

--- why.js
+++ why.js
@@ -1,36 +1,47 @@
-/*chrome.webNavigation.onBeforeNavigate.addListener(function(e){
-	console.log("going to facebook");
-	alert("TESTa");
-}, {url: [{hostContains: '.facebook.'}]});*/
+var answeredTabs = [];
+const retriggerThreshold = 5;
+var urlChanges = 0; //if exceeds retriggerThreshold, then retrigger WHY
+var reasons = [];
 
-var answered = [];
-
+function extLog(logLine)
+{
+	chrome.extension.getBackgroundPage().console.log(logLine);
+}
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 	var url = changeInfo.url;
-	console.log("got here");
 	if(url)
 	{
 		var badurls = ["facebook","reddit"];
 		for (i = 0; i < 3; i++)
 		{			
-			if (url.indexOf(badurls[i]) >= 0 && answered.indexOf(tabId) == -1) onWhyTab(tabId);
+			if (url.indexOf(badurls[i]) >= 0)
+			{
+				urlChanges++;
+				extLog(urlChanges);
+				if (answeredTabs.indexOf(tabId) == -1 || urlChanges >= retriggerThreshold) onWhyTab(tabId);
+			}
 		}
 		
 	} 
 });
 
-
 function onWhyTab(tabId){
-var whywhy = "WHY? ";
-for (i = 0; i < 200;  i++) whywhy += "WHY? "
-var reason = prompt(whywhy,"");
-if (reason == null || reason == "")
-{
-	chrome.tabs.getCurrent(function(tab)
+	var whywhy = "WHY? ";
+	for (i = 1; i < 666;  i++) whywhy += "WHY? "
+	var reason = prompt(whywhy);
+	if (reason == null || reason == "")
 	{
-		chrome.tabs.remove(tabId);
-	})
-	console.log("tried to close");
-}
-else answered.push(tabId);
+		chrome.tabs.getCurrent(function(tab)
+		{
+			chrome.tabs.remove(tabId);
+		});
+		console.log("Closing tab");
+	}
+	else
+	{ 
+		answeredTabs.push(tabId);
+		reasons.push(reason);
+		extLog(reasons);
+		urlChanges = 0;
+	}
 }
